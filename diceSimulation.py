04d67d17fd1cfecea912b5c62b2348a33a7d2173
Frank Huang <FrankHuang2013@outlook.com>
2014-11-07 09:13:00 -0500

    Random choose from 1-6

--- diceSimulation.py
+++ diceSimulation.py
@@ -1,2 +1,7 @@
 #! /usr/bin/env python
-# "This is a dice simulation program."
+# "This is a dice simulation program. Written by Frank Huang."
+
+import random
+
+answer = random.randint(1,6)
+print("The dice shows: " + str(answer))

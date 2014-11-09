96906cae034becbe6db9e5f9348d68527dd35089
Frank Huang <FrankHuang2013@outlook.com>
2014-11-09 10:22:57 -0500

    Allow user to have different number of faces on different dices

--- diceSimulation.py
+++ diceSimulation.py
@@ -5,8 +5,8 @@ import random
 import specialInput
 
 number = specialInput.int_inputdice("How many dice would you like to roll?")
-side = specialInput.int_inputside("How many sides on your dice?")
 
 for i in range(1, number+1): 
+	side = specialInput.int_inputside("\nHow many sides on dice #{}?".format(i))
 	answer = random.randint(1,side)
 	print("Dice {} shows: {}".format(i, answer))

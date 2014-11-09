5dbb6b2ed6914c571366561f17057abeda498344
Frank Huang <FrankHuang2013@outlook.com>
2014-11-09 09:24:59 -0500

    Enable the user to input how many dices they want to roll

--- diceSimulation.py
+++ diceSimulation.py
@@ -4,6 +4,9 @@
 import random
 import specialInput
 
-side = specialInput.int_input("How many sides on your dice?")
-answer = random.randint(1,side)
-print("The dice shows: " + str(answer))
+number = specialInput.int_inputdice("How many dice would you like to roll?")
+side = specialInput.int_inputside("How many sides on your dice?")
+
+for i in range(1, number+1): 
+	answer = random.randint(1,side)
+	print("Dice {} shows: {}".format(i, answer))
